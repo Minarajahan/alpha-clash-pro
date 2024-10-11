ad573b81c661e5fd25d6893964dbac41b840b2e1
Minarajahan <meherinsultanaminara@gmail.com>
2024-10-11 20:04:58 +0800

    update

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -12,6 +12,55 @@
     
 // }
 
+function handleKyboardPress(event){
+    const playerPressed = event.key;
+
+    // stop the game ove if prees ESC
+    if(playerPressed==='Escape'){
+        gameOver();
+    }
+
+    // get the expected to press
+    const currentAlphabetElement= document.getElementById('current-alphabet');
+    const currentAlphabet=currentAlphabetElement.innerText;
+    const expectedAlphabet=currentAlphabet.toLowerCase();
+// cheak right or wrong press
+if(playerPressed===expectedAlphabet){
+    console.log('you got a point')
+
+    const currentScore=getTextElemenValuetById('current-score');
+    const updatedScore=currentScore+1;
+    setTextElementById('current-score',updatedScore);
+   
+   
+    // update score
+    // const currentScoreElement=document.getElementById('current-score');
+    // const currentScoreText=currentScoreElement.innerText;
+    // const currentScore=parseInt(currentScoreText);
+    // // new score
+    
+    // // show the update score
+    // currentScoreElement.innerText=newScore;
+
+
+    removeBackgroundColor(expectedAlphabet);
+    continueGame();
+}
+else{
+    console.log('You got wrong point');
+    const currentLife=getTextElemenValuetById('current-life');
+    const updatedLife=currentLife-1;
+    setTextElementById('current-life',updatedLife);
+
+    if(updatedLife===0){
+        gameOver();
+    }
+
+}
+}
+
+document.addEventListener('keyup',handleKyboardPress);
+
 function continueGame(){
     // generate alphabet
 
@@ -28,7 +77,23 @@ function continueGame(){
 
 function play(){
     hideElement('home-screen');
+    hideElement('final-score');
     showElement('play-ground');
+    // reset life
+    setTextElementById('current-life',5);
+    setTextElementById('current-score',0);
+
     continueGame();
 }
 
+function gameOver(){
+    hideElement('play-ground');
+    showElement('final-score');
+    // update final score
+    const lastScore=getTextElemenValuetById('current-score');
+    setTextElementById('last-score',lastScore);
+
+    // clear the last selected bg
+    const currentAlphabet=getElementTextById('current-alphabet');
+    removeBackgroundColorById(currentAlphabet);
+}
--- scripts/utility.js
+++ scripts/utility.js
@@ -20,6 +20,23 @@ function removeBackgroundColor(elementId){
     const element =document.getElementById(elementId);
     element.classList.remove('bg-orange-400');
 }
+function getTextElemenValuetById(elementId){
+    const element=document.getElementById(elementId);
+    const elementValueText=element.innerText;
+    const value =parseInt(elementValueText);
+    return value;
+}
+function setTextElementById(elementId,value){
+    const element=document.getElementById(elementId);
+    element.innerText=value;
+}
+
+function getElementTextById(elementId){
+    const element=document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+}
+
 
 // alphabet 
 
